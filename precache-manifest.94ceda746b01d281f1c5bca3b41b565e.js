037f0dd7895352b6130487c98f01b605fd52d98d
IanSoo <soozhenian@gmail.com>
2020-11-01 14:39:26 +0900

    Updates

---
+++ precache-manifest.94ceda746b01d281f1c5bca3b41b565e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae732a91971422ba3f11357fa6523753",
+    "url": "/bestby-test/index.html"
+  },
+  {
+    "revision": "e949b2e867a46949f96f",
+    "url": "/bestby-test/static/css/main.75082938.chunk.css"
+  },
+  {
+    "revision": "55ae4b16116852452c7e",
+    "url": "/bestby-test/static/js/2.6b6aad5d.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/bestby-test/static/js/2.6b6aad5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e949b2e867a46949f96f",
+    "url": "/bestby-test/static/js/main.8124e3ee.chunk.js"
+  },
+  {
+    "revision": "d93822679b102b4cae0f",
+    "url": "/bestby-test/static/js/runtime-main.34d0070f.js"
+  },
+  {
+    "revision": "b889583a98c39fbf0714bacc049216ea",
+    "url": "/bestby-test/static/media/hire.b889583a.svg"
+  },
+  {
+    "revision": "6bc795d95df9f6e50f2fcdd660b83d18",
+    "url": "/bestby-test/static/media/man-suitcase.6bc795d9.jpg"
+  },
+  {
+    "revision": "5e3e549f239ac87e2cbb53a9e10d9422",
+    "url": "/bestby-test/static/media/profile.5e3e549f.svg"
+  },
+  {
+    "revision": "55cfb4817b9e054491bd9dafc21087c2",
+    "url": "/bestby-test/static/media/svg-1.55cfb481.svg"
+  },
+  {
+    "revision": "d4846e1163a89864dd49fb03ec6308ed",
+    "url": "/bestby-test/static/media/svg-2.d4846e11.svg"
+  },
+  {
+    "revision": "397c455a49d4f872c292a3dc25432579",
+    "url": "/bestby-test/static/media/svg-3.397c455a.svg"
+  },
+  {
+    "revision": "0519cb26d4cc7cc1a2e68fc3ad3ad0f7",
+    "url": "/bestby-test/static/media/swipe.0519cb26.svg"
+  }
+]);
