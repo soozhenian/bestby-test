86b1d25d082cdc5f6780a7e6dd95187c53001241
IanSoo <soozhenian@gmail.com>
2020-11-30 15:09:24 +0900

    new slug

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App(){
       <ScrollToTop />
       <Navbar />
       <Switch>
-        <Route path='/123' exact component={Home} />
+        <Route path='/' exact component={Home} />
         <Route path='/services' component={Services} />
         <Route path='/products' component={Products} />
         <Route path='/sign-up' component={SignUp} />
